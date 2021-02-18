3ccec4fd42ca0b04ffaee66b8c989b5f169af83c
yeseo-ko <w44kpo@naver.com>
2021-02-18 22:45:45 +0900

    메뉴바 제이쿼리 적용, 스터디 대신 스킬로 대체, 버튼 디자인 적용

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
-    $('.responsive').slick({
+    $('.projects').slick({
         dots: true,
         infinite: true,
         speed: 300,
-        slidesToShow: 3,
+        slidesToShow: 3.2,
         slidesToScroll: 4,
         autoplay:true,
         autoplaySpeed: 1000,
@@ -12,8 +12,8 @@ $(document).ready(function () {
           {
             breakpoint: 1024,
             settings: {
-              slidesToShow: 3,
-              slidesToScroll: 3,
+              slidesToShow: 1,
+              slidesToScroll: 1,
               infinite: true,
               dots: true
             }
@@ -21,8 +21,8 @@ $(document).ready(function () {
           {
             breakpoint: 600,
             settings: {
-              slidesToShow: 2,
-              slidesToScroll: 2
+              slidesToShow: 1,
+              slidesToScroll: 1,
             }
           },
           {
